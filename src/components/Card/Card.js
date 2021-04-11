aaa52a83d1e6f1023071683fd1dab0932a9ec2cd
Alexsandra-lima <alexsandramlimam@gmail.com>
2021-04-11 18:26:22 -0300

    iniciando

---
+++ src/components/Card/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import typeColors from '../../helpers/typeColors'
+import './style.css';
+
+function Card({ pokemon }) {
+    return (
+        <div className='Card'>
+            <div className='Card_img'>
+                <img src={pokemon.sprites.front_default} alt='' />
+            </div>
+            <div className="Card_Name">{pokemon.name}</div>
+
+            <div className="Card_types">
+                {pokemon.types.map(type => {
+                    return (
+                        <div className="Card_type" style={{ backgroundColor: typeColors[type.type.name] }}>
+                            {type.type.name}
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="Card_info">
+                <div className="Card_data Card_data--weight">
+                    <p className="title">Weight</p>
+                    <p>{pokemon.weight}</p>
+                </div>
+                <div className='Card_data Card_data--height'>
+                    <p className='title'>Height</p>
+                    <p>{pokemon.height}</p>
+                </div>
+                <div className='Card_data Card_data--ability'>
+                    <p className='title'>Ability</p>
+                    <p>{pokemon.abilities[0].ability.name}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+
+
+
+function Navbar() {
+
+      return (
+            <div className="Navbar">
+                  Pokemon
+                
+            </div>
+           
+      );
+
+
+}
+
+
+export default Navbar;
